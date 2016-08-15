964600cbd4e453ccb67f7abbc37951c56dff86fc
Richard Ma <richard.ma.19850509@gmail.com>
2016-08-16 01:24:59 +0800

    all done TODO: add product size and sku

--- app/addons/order_address_list_exporter/controllers/backend/exporter.php
+++ app/addons/order_address_list_exporter/controllers/backend/exporter.php
@@ -58,6 +58,30 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_REQUEST['order_ids'])) {
 
     } elseif ($mode == 'orders') {
         // export orders
+        require_once Registry::get('config.dir.lib') . "vendor/PHPExcel/Classes/PHPExcel.php";
+
+        //Create new PHPExcel object
+        $objPHPExcel = new PHPExcel();
+                    
+        // Set document properties
+        $objPHPExcel->getProperties()->setCreator("Richard Ma")
+                ->setLastModifiedBy("Richard Ma")
+                ->setTitle("Office 2007 XLSX Test Document")
+                ->setSubject("Office 2007 XLSX Test Document")
+                ->setDescription("Test document for Office 2007 XLSX, generated using PHP classes.")
+                ->setKeywords("office 2007 openxml php")
+                ->setCategory("Test result file");
+                                                                                                                                                                                                                                                                       
+        // Set active sheet index to the first sheet, so Excel opens this as the first sheet
+        $objPHPExcel->setActiveSheetIndex(0);
+
+        $objPHPExcel->getActiveSheet()->getColumnDimension('B')->setWidth(40);
+        $objPHPExcel->getActiveSheet()->getColumnDimension('C')->setAutoSize(true);
+
+        // Add some data
+        $delta = 6;
+        $start = 1;
+
         foreach ($orders as $order) {
             $data['order_id'] = $order['order_id'];
             $data['name'] = $order['firstname'] . ' ' . $order['lastname'];
@@ -67,7 +91,71 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_REQUEST['order_ids'])) {
             $data['country'] = $order['s_country'];
             $data['post'] = $order['s_zipcode'];
             $data['tel'] = $order['phone'];
+
+            //fn_print_r($order);
+            foreach($order['products'] as $product) {
+                //fn_print_r($product);
+
+                $product_id = $product['product_id'];
+                $icon = fn_get_cart_product_icon($product_id);
+                $image = $icon['detailed']['image_path'];
+                //fn_print_r($image); // print product image path
+
+        		$end = $start + $delta - 1;
+                $objPHPExcel->getActiveSheet()
+                        ->mergeCells('A'.$start.':A'.$end.'')
+                        ->setCellValue('A'.$start.'', $data['order_id'])
+    
+                        ->setCellValue('B'.$start.'', 'size: ' . 'Product Size') //TODO
+                        ->mergeCells('B'.(string)($start+1).':B'.$end.'')
+
+                        ->setCellValue('C'.$start.'', $data['name'])
+                        ->setCellValue('C'.(string)($start + 1).'', $order['s_address'])
+                        ->setCellValue('C'.(string)($start + 2).'', $order['s_address_2'])
+                        ->setCellValue('C'.(string)($start + 3).'', $data['city']. ', ' .$data['province']. ' ' .$data['post'])
+                        ->setCellValue('C'.(string)($start + 4).'', $data['country'])
+                        ->setCellValue('C'.(string)($start + 5).'', $data['tel'])
+
+                        ->setCellValue('D'.$start.'', $product['product'])
+                        ->setCellValue('D'.(string)($start + 1).'', 'Qty: '.$product['amount'])
+                        ->setCellValue('D'.(string)($start + 2).'', 'SKU: '.'Product SKU') //TODO
+
+                        ->getStyle('C'.(string)($start + 5))->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_LEFT);
+                // add picture
+                $imageUrl = strtok($image, '?'); // only get url without params
+                $imagePath = str_replace(pathinfo(fn_url())['dirname'], Registry::get('config.dir.root'), $imageUrl);
+                //fn_print_r($imagePath); // test image path
+                
+                $objDrawing = new PHPExcel_Worksheet_Drawing();
+                $objDrawing->setPath($imagePath);
+                $objDrawing->setCoordinates('B'.(string)($start+1));
+                $objDrawing->setHeight(80);
+                $objDrawing->setWorksheet($objPHPExcel->getActiveSheet());
+
+                $start = $start + $delta;
+            }
         }
+
+        // Rename worksheet
+        $objPHPExcel->getActiveSheet()->setTitle('Orders');
+        
+        // Redirect output to a client’s web browser (Excel2007)
+        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        header('Content-Disposition: attachment;filename="orders-'.date('Y_m_d_H_i_s').'.xlsx"');
+        header('Cache-Control: max-age=0');
+        // If you're serving to IE 9, then the following may be needed
+        header('Cache-Control: max-age=1');
+        
+        // If you're serving to IE over SSL, then the following may be needed
+        header ('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+        header ('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT'); // always modified
+        header ('Cache-Control: cache, must-revalidate'); // HTTP/1.1
+        header ('Pragma: public'); // HTTP/1.0
+        
+        $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+        $objWriter->save('php://output');
+
+        exit;
     }
 
     //return array(CONTROLLER_STATUS_REDIRECT, 'exporter.index'); // redirect to exporter.index
--- app/addons/order_address_list_exporter/schemas/menu/menu.post.php
+++ app/addons/order_address_list_exporter/schemas/menu/menu.post.php
@@ -1,18 +1,8 @@
 <?php
 
 $schema['top']['addons']['items']['order_address_list_exporter'] = array(
-    'href' => 'exporter.address_list',
+    'href' => 'exporter.index',
     'position' => 99,
-);
-
-$schema['top']['addons']['items']['order_exporter'] = array(
-    'href' => 'exporter.orders',
-    'position' => 100,
-);
-
-$schema['top']['addons']['items']['error_test'] = array(
-    'href' => 'exporter.none',
-    'position' => 101,
 );
 
 return $schema;
